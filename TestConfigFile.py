d1af693056406d160532e7895aa8845aa71b7c68
adamyoung600 <adamyoung600@gmail.com>
2016-02-14 16:42:42 -0500

    Add config file functionality for monitored params and shift light thresholds.

--- TestConfigFile.py
+++ TestConfigFile.py
@@ -5,4 +5,13 @@ Test functionality of the config file reading/writing.
 """
 
 parser = Config()
-parser.writeFile()
+parser.setDefaultConfig()
+
+parser.setMonitoredParams(["P1", "P36", "E3"])
+parser.setRpmThresholds([200,400,66,234])
+
+temp = parser.getMonitoredParams()
+print temp
+
+temp = parser.getRpmThresholds()
+print temp
--- Util/Config.py
+++ Util/Config.py
@@ -4,18 +4,20 @@ class Config():
 
     def __init__(self):
         self.__parser = ConfigParser.RawConfigParser()
+        self.parseFile()
 
     def parseFile(self):
         self.__parser.read('config.ini')
 
-    def writeFile(self):
-        self.__parser.add_section('MonitoredParams')
-        self.__parser.set('MonitoredParams', 'param1', 'boost')
-        self.__parser.set('MonitoredParams', 'param2', 'test')
-        self.__parser.set('MonitoredParams', 'param3', 'test1')
-        self.__parser.set('MonitoredParams', 'param4', 'test2')
+    def setDefaultConfig(self):
+        if not self.__parser.has_section('MonitoredParams'):
+            self.__parser.add_section('MonitoredParams')
+        self.__parser.set('MonitoredParams', 'param1', 'P2')
+        self.__parser.set('MonitoredParams', 'param2', 'P200')
+        self.__parser.set('MonitoredParams', 'param3', 'E20')
 
-        self.__parser.add_section('RpmThresholds')
+        if not self.__parser.has_section('RpmThresholds'):
+            self.__parser.add_section('RpmThresholds')
         self.__parser.set('RpmThresholds', 'green', '3500')
         self.__parser.set('RpmThresholds', 'orange', '4500')
         self.__parser.set('RpmThresholds', 'red', '5500')
@@ -24,3 +26,32 @@ class Config():
         with open('./Util/config.ini', 'wb') as configFile:
             self.__parser.write(configFile)
 
+    def setMonitoredParams(self, params):
+        self.__parser.set('MonitoredParams', 'param1', params[0])
+        self.__parser.set('MonitoredParams', 'param2', params[1])
+        self.__parser.set('MonitoredParams', 'param3', params[2])
+        with open('./Util/config.ini', 'wb') as configFile:
+            self.__parser.write(configFile)
+
+    def getMonitoredParams(self):
+        monitoredParams = []
+        monitoredParams.append(self.__parser.get('MonitoredParams', 'param1'))
+        monitoredParams.append(self.__parser.get('MonitoredParams', 'param2'))
+        monitoredParams.append(self.__parser.get('MonitoredParams', 'param3'))
+        return monitoredParams
+
+    def setRpmThresholds(self, thresholds):
+        self.__parser.set('RpmThresholds', 'green', thresholds[0])
+        self.__parser.set('RpmThresholds', 'orange', thresholds[1])
+        self.__parser.set('RpmThresholds', 'red', thresholds[2])
+        self.__parser.set('RpmThresholds', 'blue', thresholds[3])
+        with open('./Util/config.ini', 'wb') as configFile:
+            self.__parser.write(configFile)
+
+    def getRpmThresholds(self):
+        thresholds = []
+        thresholds.append(self.__parser.get('RpmThresholds', 'green'))
+        thresholds.append(self.__parser.get('RpmThresholds', 'orange'))
+        thresholds.append(self.__parser.get('RpmThresholds', 'red'))
+        thresholds.append(self.__parser.get('RpmThresholds', 'blue'))
+        return thresholds
