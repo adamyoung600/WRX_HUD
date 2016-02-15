f1828859be0f06f1c466b8a50f49b7bbe04d4117
adamyoung600 <adamyoung600@gmail.com>
2016-02-15 16:25:10 -0500

    Add switch panel placeholder

---
+++ Hardware/Input/SwitchPanel.py
@@ -0,0 +1,12 @@
+class SwitchPanel():
+
+    def __init__(self):
+        pass
+
+    def isShiftLightEnabled(self):
+        #TODO: Implement this to change on GPIO interrupt rising/falling edge.
+        return True
+
+    def isLCDEnabled(self):
+        #TODO: Implement this to change on GPIO interrupt rising/falling edge.
+        return True
