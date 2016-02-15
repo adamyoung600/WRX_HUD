ba356d746beb3c3b210cd210e718bc7eca69dcb0
adamyoung600 <adamyoung600@gmail.com>
2016-02-15 16:24:32 -0500

    Implemented shift lights interface

---
+++ Hardware/ShiftLights.py
@@ -0,0 +1,58 @@
+import RPi.GPIO as GPIO
+
+class ShiftLights():
+
+    def __init__(self, thresholds):
+        self._greenLed = 21
+        self._orangeLed = 22
+        self._redLed = 23
+        self._blueLed = 24
+        GPIO.setup(self._greenLed, GPIO.OUTPUT)
+        GPIO.setup(self._orangeLed, GPIO.OUTPUT)
+        GPIO.setup(self._redLed, GPIO.OUTPUT)
+        GPIO.setup(self._blueLed, GPIO.OUTPUT)
+        self._anyLightsOn = False
+        self.clearLights()
+        self.setThresholds(thresholds)
+
+
+
+    def setThresholds(self, thresholds):
+        self._greenThreshold = thresholds[0]
+        self._redThreshold = thresholds[1]
+        self._orangeThreshold = thresholds[2]
+        self._blueThreshold = thresholds[3]
+
+    def _clearLights(self):
+        GPIO.output(self._greenLed, False)
+        GPIO.output(self._orangeLed, False)
+        GPIO.output(self._redLed, False)
+        GPIO.output(self._blueLed, False)
+        self._anyLightsOn = False
+
+    def update(self, rpm):
+        if rpm < self._greenThreshold and not self._anyLightsOn:
+            return
+        elif rpm < self._greenThreshold and self._anyLightsOn:
+            self._clearLights()
+            return
+
+        if rpm > self._greenThreshold:
+            GPIO.output(self._greenLed, True)
+        else:
+            GPIO.output(self._greenLed, False)
+
+        if rpm > self._orangeThreshold:
+            GPIO.output(self._orangeLed, True)
+        else:
+            GPIO.output(self._orangeLed, False)
+
+        if rpm > self._redThreshold:
+            GPIO.output(self._redLed, True)
+        else:
+            GPIO.output(self._redLed, False)
+
+        if rpm > self._blueThreshold:
+            GPIO.output(self._blueLed, True)
+        else:
+            GPIO.output(self._blueLed, False)
