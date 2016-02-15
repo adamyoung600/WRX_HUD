5da782d5bdf577cf2d0262067e3f345d37596d43
adamyoung600 <adamyoung600@gmail.com>
2016-02-14 21:58:39 -0500

    Add ability to set param titles on DataDisplay.

--- DataDisplay.py
+++ DataDisplay.py
@@ -1,7 +1,18 @@
 
+from Hardware.WideHKOLED.WideHKOLED import WideHKOLED
+from Util.ParameterAbbreviations import abbreviations
+
 class DataDisplay():
 
     def __init__(self):
-        print "Test"
+        self._lcd = WideHKOLED
+
+    def setDisplayTitles(self, paramIDs):
+        for i in range(3):
+            title = "None"
+            if paramIDs[i] in abbreviations:
+                title = abbreviations('paramIDs[i]')
+            self._lcd.sendString(title, 0, i*6)
+
 
 
