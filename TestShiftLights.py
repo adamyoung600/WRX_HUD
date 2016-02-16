2d63c930906bda64e23dc12e331b2e6ea1865960
adamyoung600 <adamyoung600@gmail.com>
2016-02-15 20:52:42 -0500

    Rework Shift Lights test to use integers

--- TestShiftLights.py
+++ TestShiftLights.py
@@ -5,23 +5,23 @@ lights = ShiftLights([3500, 4000, 5000, 6000])
 
 while True:
     print "Sending 3000"
-    lights.update("3000")
+    lights.update(3000)
     time.sleep(1)
 
     print "Sending 3700"
-    lights.update("3700")
+    lights.update(3700)
     time.sleep(1)
 
     print "Sending 4500"
-    lights.update("4500")
+    lights.update(4500)
     time.sleep(1)
 
     print "Sending 5500"
-    lights.update("5500")
+    lights.update(5500)
     time.sleep(1)
 
     print "Sending 6500"
-    lights.update("6500")
+    lights.update(6500)
     time.sleep(1)
 
     print "Clearing lights"
