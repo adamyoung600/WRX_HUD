f677be5891f24571dd6315e5c6eff3306c96cfe7
adamyoung600 <adamyoung600@gmail.com>
2016-03-07 19:46:55 -0500

    Fixed monitored params name in MenuContextMain

--- MenuSystem/MenuContextMain.py
+++ MenuSystem/MenuContextMain.py
@@ -7,7 +7,7 @@ class MenuContextMain(MenuContext):
     def __init__(self, inManager, inLcd):
         super(MenuContextMain, self).__init__(inManager, inLcd)
         self.title = "Main"         #Title to display at the top of the display when this menu is active.
-        self.entries = ["Monitored Parameters", "Peak Boost", "Trouble Codes", "Settings"]           #Holds a reference to all the possible entries in the menu
+        self.entries = ["Monitored Params", "Peak Boost", "Trouble Codes", "Settings"]           #Holds a reference to all the possible entries in the menu
 
     """
     Callback methods that perform an action for a button press based on which menu the user is in.
