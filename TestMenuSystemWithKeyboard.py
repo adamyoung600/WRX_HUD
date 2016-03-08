a934237a5ee58653a7e20a246a5a19d6bf7aca41
adamyoung600 <adamyoung600@gmail.com>
2016-03-07 19:31:04 -0500

    Fix import problem in MenuSystem Test

--- TestMenuSystemWithKeyboard.py
+++ TestMenuSystemWithKeyboard.py
@@ -1,12 +1,13 @@
 from MenuSystem.MenuManager import *
 from Hardware.SH1106.SH1106LCD import *
 from Hardware.Input.Keyboard import *
+from HUDMain import HUDMain
 
 
 
 
 lcd = SH1106LCD()
-menuSystem = MenuManager(lcd)
+menuSystem = MenuManager(lcd, HUDMain)
 menuSystem.initiateDisplay()
 menuSystem.enterMenuMode()
 keyboard = Keyboard()
