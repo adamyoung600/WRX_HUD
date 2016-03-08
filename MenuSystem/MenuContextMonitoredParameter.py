ee171b470f457a379cb22844220005b8e6fb503c
adamyoung600 <adamyoung600@gmail.com>
2016-03-07 19:48:53 -0500

    Fix entry list in monitored params context

--- MenuSystem/MenuContextMonitoredParameter.py
+++ MenuSystem/MenuContextMonitoredParameter.py
@@ -7,7 +7,7 @@ class MenuContextMonitoredParameter(MenuContext):
     def __init__(self, inManager, inLcd):
         super(MenuContextMonitoredParameter,self).__init__(inManager, inLcd)
         self.title = "Monitored Params"     #Title to display at the top of the display when this menu is active.
-        self.entries = ["Set Parameters, Set Passive Mode"]           #Holds a reference to all the possible entries in the menu
+        self.entries = ["Set Parameters", "Set Passive Mode"]           #Holds a reference to all the possible entries in the menu
         self.parent = "Main"
 
 
